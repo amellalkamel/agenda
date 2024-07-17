b173460492964b9b38fdc3b211abef1170821892
Kamatou <amellalkameleddine@gmail.com>
2024-07-17 14:19:53 +0200

    coller le script de matomo

--- src/GoogleTagManager.js
+++ src/GoogleTagManager.js
@@ -1,27 +1,17 @@
 import { useEffect } from 'react';
 
-const GoogleTagManager = ({ gtmId }) => {
+const GoogleTagManager = () => {
   useEffect(() => {
-    // Replace with your Matomo URL and Site ID
-    const matomoUrl = 'https://phedre.matomo.cloud/';
-    const siteId = '2';
-
-    if (!window._paq) {
-      window._paq = [];
-    }
-
-    window._paq.push(['trackPageView']);
-    window._paq.push(['enableLinkTracking']);
+    var _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
     (function() {
-      var u = matomoUrl;
-      window._paq.push(['setTrackerUrl', u + 'matomo.php']);
-      window._paq.push(['setSiteId', siteId]);
-      var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
-      g.type = 'text/javascript';
-      g.async = true;
-      g.defer = true;
-      g.src = u + 'matomo.js';
-      s.parentNode.insertBefore(g, s);
+      var u="https://phedre.matomo.cloud/";
+      _paq.push(['setTrackerUrl', u+'matomo.php']);
+      _paq.push(['setSiteId', '2']);
+      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+      g.async=true; g.src='https://cdn.matomo.cloud/phedre.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
     })();
   }, []);
 
