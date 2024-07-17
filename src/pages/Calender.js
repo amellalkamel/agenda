be15feed334b3dc098546c45df802cd28648dc2b
Kamatou <amellalkameleddine@gmail.com>
2024-07-17 16:48:26 +0200

    add click to button

--- src/pages/Calender.js
+++ src/pages/Calender.js
@@ -5,10 +5,13 @@ export default function Calender() {
     /**
      * List des horaires (un tableau de 13 elements)
      */
+    const handlClick = () => {
+        console.log('handlClick event fired');
+    };
     const hours = ['9 am', '10 am', '11 am', '12 am', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', '8 pm', '9 pm'];
     return (
         <div className='body-calender'>
-            <button onClick="dataLayer.push({'event': 'hello'})">test</button>
+            <button onClick={handlClick}>test</button>
             {hours.map((hour, index) =>
             (
                 <div key={index} className="hourContainer">
