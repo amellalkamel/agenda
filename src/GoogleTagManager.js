94c18c360c89472b6d617b3e76df2d865879d5ba
Kamatou <amellalkameleddine@gmail.com>
2024-07-17 14:39:17 +0200

    youtube  matomo tag manager

--- src/GoogleTagManager.js
+++ src/GoogleTagManager.js
@@ -4,11 +4,12 @@ const GoogleTagManager = ({ gtmId }) => {
     useEffect(() => {
       const script = document.createElement('script');
       script.innerHTML = `
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${gtmId}');
+      var _mtm = window._mtm = window._mtm || [];
+      _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+      (function() {
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.async=true; g.src='https://cdn.matomo.cloud/phedre.matomo.cloud/container_dVlRHDco.js'; s.parentNode.insertBefore(g,s);
+      })();
       `;
       document.head.appendChild(script);
   
